8abfdf9b142369db3a8a00a7f860a01ed4efbf0c
Jaynti Kanani <jdkanani@gmail.com>
2018-06-09 19:47:31 +0530

    fix build

--- contracts/RootChainInterface.sol
+++ contracts/RootChainInterface.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.23;
 
 
 interface RootChainInterface {
+  function getHeaderBlock(uint256) public view returns (bytes32, uint256, uint256, uint256);
+  function networkId() public view returns (uint256);
   function chain() public view returns (bytes32);
   function slash() public;
 }
