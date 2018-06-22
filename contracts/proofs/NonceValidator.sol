46dacabeee9c563f67bcb365342496405d99fa42
Jaynti Kanani <jdkanani@gmail.com>
2018-06-22 17:37:53 +0530

    nonce mismatch proof

--- contracts/proofs/NonceValidator.sol
+++ contracts/proofs/NonceValidator.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.4.23;
 
-
+import "../lib/SafeMath.sol";
 import "../lib/RLP.sol";
 import "../mixin/RootChainValidator.sol";
 
 
 contract NonceValidator is RootChainValidator {
-   function validateDuplicateNonce(
+  using SafeMath for uint256;
+  using RLP for bytes;
+  using RLP for RLP.RLPItem;
+  using RLP for RLP.Iterator;
+
+  function validateMisMatchedNonce(
     bytes tx1,
     bytes tx2
   ) {
@@ -34,6 +39,7 @@ contract NonceValidator is RootChainValidator {
 
     address sender = getTxSender(txData[7].toData());
     uint256 nonce = txData[7].toList()[0].toUint(); // fetch nonce
+    uint256 txIndex = txData[2].toUint().mul(10000000).add(getPathInt(txData[6])); // blockNumber * 10000000 + tx index in block 
 
     // validate second transaction
     txData = tx2.toRLPItem().toList();
@@ -56,13 +62,22 @@ contract NonceValidator is RootChainValidator {
 
     // check if sender is the same in both transactions
     require(getTxSender(txData[7].toData()) == sender);
-    // check if both nonce values are same, just call slasher
-    if (txData[7].toList()[0].toUint() == nonce) {
+    // make sure tx2 is after tx1
+    require (
+      txData[2].toUint().mul(10000000).add(getPathInt(txData[6])) > txIndex
+    );
+
+    // check if both nonce values are same or nonce2 < nonce1, just call slasher
+    if (txData[7].toList()[0].toUint() <= nonce) {
       rootChain.slash();
       return;
     }
 
     // revert the operation
     revert('Invalid nonce challenge');
+  }
+
+  function getPathInt(RLP.RLPItem path) internal view returns (uint256) {
+    return path.toData().toRLPItem().toData().toRLPItem().toUint();
   }
 }
