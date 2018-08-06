af287b95ddf7b28558c770b0fe566e9f314ec70f
Jaynti Kanani <jdkanani@gmail.com>
2018-08-06 19:33:20 +0530

    parity fix

--- test-blockchain/start.sh
+++ test-blockchain/start.sh
@@ -2,6 +2,7 @@
 
 CWD=$PWD
 
-nohup parity --config config.toml > $CWD/data/node.log 2>&1 &
-echo $! > $CWD/data/node.pid
-echo "Node started. Check test-blockchain/data/node.log for more logs"
+# nohup parity --config $CWD/config.toml > $CWD/data/node.log 2>&1 &
+# echo $! > $CWD/data/node.pid
+# echo "Node started. Check test-blockchain/data/node.log for more logs"
+parity --config $CWD/config.toml
