25227ceab406936fde33e72a3041b06825da7814
Jaynti Kanani <jdkanani@gmail.com>
2018-08-06 19:26:35 +0530

    parity fix

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -13,56 +13,30 @@ cleanup() {
   echo "Cleaning up"
   pkill -f geth
   pkill -f ganache-cli
-  # Kill the testrpc instance that we started (if we started one and if it's still running).
-  if [ -n "$testrpc_pid" ] && ps -p $testrpc_pid > /dev/null; then
-    kill -9 $testrpc_pid
-  fi
-
-  # Kill the geth instance that we started (if we started one and if it's still running).
-  if [ -n "$geth_pid" ] && ps -p $geth_pid > /dev/null; then
-    kill -9 $geth_pid
-  fi
-
-  rm -rf $PWD/test-blockchain/geth
   echo "Done"
-}
-
-testrpc_port=8545
-testrpc_running() {
-  nc -z localhost "$testrpc_port"
-}
-
-geth_port=8546
-geth_running() {
-  nc -z localhost "$geth_port"
 }
 
 start_testrpc() {
   npm run testrpc > /dev/null &
-  testrpc_pid=$!
 }
 
 start_blockchain() {
-  if [ ! -d "$PWD/test-blockchain/geth" ]; then
-    geth --datadir "$PWD/test-blockchain" init "$PWD/test-blockchain/genesis.json"
-  fi
-  geth --datadir "$PWD/test-blockchain" --targetgaslimit '900000000000000' --rpc --rpcport 8546 --rpccorsdomain "*" --wsorigins "*" --unlock "0x9fb29aac15b9a4b7f17c3385939b007540f4d791,0xacf8eccdca12a0eb6ae4fb1431e26c44e66decdb" --password "$PWD/scripts/password" --networkid 13 --mine > /dev/null &
-  geth_pid=$!
+  # if [ ! -d "$PWD/test-blockchain/geth" ]; then
+  #   geth --datadir "$PWD/test-blockchain" init "$PWD/test-blockchain/genesis.json"
+  # fi
+  # geth --datadir "$PWD/test-blockchain" --targetgaslimit '900000000000000' --rpc --rpcport 8546 --rpccorsdomain "*" --wsorigins "*" --unlock "0x9fb29aac15b9a4b7f17c3385939b007540f4d791,0xacf8eccdca12a0eb6ae4fb1431e26c44e66decdb" --password "$PWD/scripts/password" --networkid 13 --mine > /dev/null &
+  # geth_pid=$!
+  cd $PWD/test-blockchain
+  bash clean.sh
+  bash start.sh
+  cd ..
 }
 
 
-if testrpc_running; then
-  echo "Using existing testrpc instance"
-else
-  echo "Starting our own testrpc instance"
-  start_testrpc
-fi
+echo "Starting our own testrpc instance"
+start_testrpc
 
-if geth_running; then
-  echo "Using existing geth instance"
-else
-  echo "Starting our own geth instance"
-  start_blockchain
-fi
+echo "Starting our own geth instance"
+start_blockchain
 
 npm run truffle:test "$@"
