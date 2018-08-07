1a2d8e3821da5fc3e0ddb659586487ef5197e54e
Jaynti Kanani <jdkanani@gmail.com>
2018-08-07 12:59:04 +0530

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: 'standard',
+  env: {
+    node: true,
+    es6: true,
+    mocha: true
+  },
   rules: {
     'space-before-function-paren': ['error', 'never']
   }
