64cb006698388b41d7e53969c6af4a3bf27724cc
ashishrp <a4ashishrp@gmail.com>
2018-09-28 15:36:04 +0530

    Queue lib for staker list queue

---
+++ contracts/lib/Queue.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./SafeMath.sol";
+
+library Queue Ownable {
+  useing SafeMath for uint256;
+  address[] items;
+  uint256 lastIndex;
+
+  function push(address item){
+    items.push(item);
+    lastIndex = lastIndex.add(1);
+  }
+
+  function pop() returns (address _item) {
+    require(lastIndex > 1);
+    _item = items[lastIndex];
+    delete items[lastIndex];
+    lastIndex = lastIndex.sub(1);
+  }
+
+  function popByIndex(uint256 index) returns (address _item) {
+    require(lastIndex > 1 && lastIndex >= index );
+    _item = items[index];
+    items[index] = items[lastIndex];
+    delete items[lastIndex];
+    lastIndex = lastIndex.sub(1);
+  }
+
+}
