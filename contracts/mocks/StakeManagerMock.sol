957d5ec4b7107e80fd0eeefcd658aac6048d66ed
ashishrp <a4ashishrp@gmail.com>
2018-10-25 14:13:03 +0530

    add modifier in mock instead of function

--- contracts/mocks/StakeManagerMock.sol
+++ contracts/mocks/StakeManagerMock.sol
@@ -8,28 +8,8 @@ contract StakeManagerMock is StakeManager {
     WITHDRAWAL_DELAY = 0;
   }
   
-  function finalizeCommit() public {
-    uint256 nextEpoch = currentEpoch.add(1);
-    // update totalstake and validator count 
-    validatorState[nextEpoch].amount = (
-      validatorState[currentEpoch].amount + validatorState[nextEpoch].amount
-    );
-    validatorState[nextEpoch].stakerCount = (
-      validatorState[currentEpoch].stakerCount + validatorState[nextEpoch].stakerCount
-    );
-
-    // remove old validators from tree
-    for (uint256 i = 0; i < validatorState[currentEpoch].exiters.length;i++) {
-      uint256 value = validatorState[currentEpoch].exiters[i];
-      if (value != 0) {
-        validatorList.deleteNode(value);
-      }
-    }
-    
-    currentValidatorSetSize = uint256(validatorState[nextEpoch].stakerCount);
-    currentValidatorSetTotalStake = uint256(validatorState[nextEpoch].amount);
-    currentEpoch = nextEpoch;
-    // erase old data/history
-    delete validatorState[currentEpoch.sub(1)];
+  modifier onlyRootChain() {
+    _;
   }
+
 }
