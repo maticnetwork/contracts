2c283b4d43ae7fd667d1270117f0ead4e06ed275
0xAshish <a4ashishrp@gmail.com>
2018-11-24 17:38:01 +0530

    update log-decoder for ethersv4

--- test/helpers/log-decoder.js
+++ test/helpers/log-decoder.js
@@ -1,32 +1,35 @@
-import ethers from 'ethers'
+import { ethers } from 'ethers'
 
 export default class LogDecoder {
   constructor(abis = []) {
     this._methodIDs = {}
+    this._interfaces = []
     abis.forEach(abi => {
-      const methodInterface = new ethers.Interface(abi)
+      const methodInterface = new ethers.utils.Interface(abi)
       Object.keys(methodInterface.events).forEach(evtKey => {
         const evt = methodInterface.events[evtKey]
-        const signature = evt.topics[0]
+        const signature = evt.topic
         // Handles different indexed arguments with same signature from different contracts
         // Like ERC721/ERC20 Transfer
         this._methodIDs[signature] = this._methodIDs[signature] || []
         this._methodIDs[signature].push(evt)
+        this._interfaces.push(methodInterface)
       })
     })
   }
-
-  decodeLogs(logs) {
+  decodeLogs(logs = []) {
     return logs.map(log => {
-      const evts = this._methodIDs[log.topics[0]]
-      for (let index = 0; index < evts.length; index++) {
-        const evt = evts[index]
+      for (let i = 0; i < this._interfaces.length; i++) {
+        // todo :please optimize me
         try {
-          return {
-            address: log.address.toLowerCase(),
-            event: evt.name,
-            signature: evt.signature,
-            args: evt.parse(log.topics, log.data)
+          const parsedLog = this._interfaces[i].parseLog(log)
+          if (parsedLog) {
+            return {
+              address: log.address.toLowerCase(),
+              event: parsedLog.name,
+              signature: parsedLog.signature,
+              args: parsedLog.values
+            }
           }
         } catch (e) {}
       }
