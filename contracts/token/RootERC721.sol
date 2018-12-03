c72f1c8e6d9408f940076c9b6ee4091f37f07f48
0xAshish <a4ashishrp@gmail.com>
2018-12-03 14:24:47 +0530

    add erc721 rootToken contract, tests

---
+++ contracts/token/RootERC721.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+import { ERC721Full } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import { ERC721Mintable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
+import { ERC721Burnable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol";
+
+
+
+contract RootERC721 is ERC721Full, ERC721Mintable, ERC721Burnable {
+
+}
