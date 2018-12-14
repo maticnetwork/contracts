bebf86e4c5c3a43168077a5eeb34b96f81f265df
0xAshish <a4ashishrp@gmail.com>
2018-12-14 17:02:31 +0530

    remove mintable/burnable

--- contracts/token/RootERC721.sol
+++ contracts/token/RootERC721.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.4.24;
 
 import { ERC721Full } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import { ERC721Mintable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
-import { ERC721Burnable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol";
 
 
-
-contract RootERC721 is ERC721Full, ERC721Mintable, ERC721Burnable {
+contract RootERC721 is ERC721Full {
   constructor (string name, string symbol) ERC721Full(name, symbol)
     public 
     {
