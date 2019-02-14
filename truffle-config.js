7dc7f9522cfc215e5fa9ddd0b27cf0afdf0f8574
0xAshish <a4ashishrp@gmail.com>
2019-02-14 17:15:24 +0530

    update ropstend gas limit

--- truffle-config.js
+++ truffle-config.js
@@ -32,7 +32,7 @@ module.exports = {
         )
       },
       network_id: 3,
-      gas: 4000000
+      gas: 8000000
     },
     kovan: {
       provider: function() {
@@ -52,7 +52,7 @@ module.exports = {
         )
       },
       network_id: 1,
-      gas: 4000000
+      gas: 8000000
     },
     matic: {
       provider: function() {
