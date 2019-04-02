9feda78c54a3cd50eb9d1f75927638180117602b
atvanguard <93arpit@gmail.com>
2019-04-02 16:58:45 +0530

    :arrow_down: truffle@4

--- truffle-config.js
+++ truffle-config.js
@@ -57,17 +57,23 @@ module.exports = {
       gas: 400000000
     }
   },
-  compilers: {
-    solc: {
-      version: '0.4.24',
-      // docker: true,
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
-      //   // evmVersion: 'byzantium'
-      }
+  // compilers: {
+  //   solc: {
+  //     version: '0.4.24',
+  //     // docker: true,
+  //     settings: {
+  //       optimizer: {
+  //         enabled: true,
+  //         runs: 200
+  //       }
+  //     //   // evmVersion: 'byzantium'
+  //     }
+  //   }
+  // },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   },
   mocha: {
