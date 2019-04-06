0764f819ef2ca4b7365c9555c9fb3aad6b6ea075
0xAshish <a4ashishrp@gmail.com>
2019-04-06 09:13:29 +0530

    add deposit and transfer(for withdraw), follow new proxy pattern

--- contracts/root/depositManager/DepositManager.sol
+++ contracts/root/depositManager/DepositManager.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.5.5;
 import { ERC20 } from "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import { ERC721 } from "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
+import { IERC721Receiver } from "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 import { Registry } from '../Registry.sol';
 import { WETH } from "../../common/tokens/WETH.sol";
 import { IDepositManager } from './IDepositManager.sol';
 import { DepositManagerStorage } from './DepositManagerStorage.sol';
 
-contract DepositManager is DepositManagerStorage, IDepositManager {
+
+contract DepositManager is DepositManagerStorage, IDepositManager, IERC721Receiver {
   string private constant TOKEN_TRANSFER_FAILED = 'Token transfer failed';
 
+  // Todo: depositEtherForUSer ?
   function depositEther()
     external
     payable
@@ -55,5 +58,37 @@ contract DepositManager is DepositManagerStorage, IDepositManager {
     internal
   {
     rootChain.createDepositBlock(_user, _token, amountOrNFTId);
+  }
+
+  function tokenFallback(address _user, uint256 _amount, bytes _data) public {
+    address _token = msg.sender;
+
+    // create deposit block with token fallback
+    depositManager.createDepositBlock(_currentHeaderBlock, _token, _user, _amount);
+  }
+
+  function onERC721Received(address operator, address from, uint256 tokenId, bytes data) public returns (bytes4) {
+    depositManager.createDepositBlock(_currentHeaderBlock, msg.sender, from, tokenId);
+    return 0x150b7a02;
+  }
+    // transfer tokens to user
+  function transferAmount(
+    address _token,
+    address _user,
+    uint256 _amount
+  ) public onlyWithdrawManager returns(bool)  {
+
+    address wethToken = depositManager.wethToken();
+
+    // transfer to user TODO: use pull for transfer
+    if (depositManager.isERC721(_token)) {
+      ERC721(_token).transferFrom(address(this), _user, _amount);
+    } else if (_token == wethToken) {
+      WETH t = WETH(_token);
+      t.withdraw(_amount, _user);
+    } else {
+      require(ERC20(_token).transfer(_user, _amount));
+    }
+    return true;
   }
 }
--- contracts/root/depositManager/DepositManagerStorage.sol
+++ contracts/root/depositManager/DepositManagerStorage.sol
@@ -4,6 +4,7 @@ import { Registry } from '../Registry.sol';
 import { RootChain } from '../RootChain.sol';
 import { ProxyStorage } from '../../common/misc/ProxyStorage.sol';
 
+
 contract DepositManagerStorage is ProxyStorage {
   Registry internal registry;
   RootChain internal rootChain;
