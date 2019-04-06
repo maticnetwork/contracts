0c23e6169e4eff0dfd088eecb31160166c3c4581
0xAshish <a4ashishrp@gmail.com>
2019-04-06 08:41:29 +0530

    restructure proxy logic
    - remove constuctor from delegated proxy

---
+++ contracts/common/misc/Proxy.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.5;
+import { DelegateProxy } from "./DelegateProxy";
+
+
+contract Proxy is ProxyStorage, DelegateProxy {
+
+  event Upgrade(address indexed newContract, bytes initializedWith);
+  event OwnerUpdate(address _prevOwner, address _newOwner);
+
+  constructor() public {
+    proxyOwner = msg.sender;
+  }
+
+  //
+  // Dispatch fallback
+  //
+
+  function () public payable {
+    // require(currentContract != 0, "If app code has not been set yet, do not call");
+    // Todo: filter out some calls or handle in the end fallback
+    delegatedFwd(proxyTo, msg.data);
+  }
+
+  //
+  // Ownership
+  //
+
+  modifier onlyProxyOwner() {
+    require(msg.sender == proxyOwner, "Unauthorized user");
+    _;
+  }
+
+  function transferOwnership(address _newOwner) public onlyProxyOwner {
+    require(_newOwner != address(0), "Empty address");
+    require(_newOwner != proxyOwner, "Already authorized");
+    proxyOwner = _newOwner;
+  }
+
+  //
+  // Upgrade
+  //
+
+  function upgrade(address _proxyTo) public onlyProxyOwner {
+    require(_proxyTo != address(0x0), "");
+    // require(isContract(_proxyTo));
+    proxyTo = _proxyTo;
+    emit Upgrade(proxyTo);
+  }
+}
--- contracts/common/misc/ProxyStorage.sol
+++ contracts/common/misc/ProxyStorage.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.5;
 
+
 contract ProxyStorage {
   address internal proxyTo;
+  address public proxyOwner;
 }
-
