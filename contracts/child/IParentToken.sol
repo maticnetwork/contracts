7acf8027a6b8d35f40776e9698eaeefc2989f683
0xAshish <a4ashishrp@gmail.com>
2019-04-13 13:48:32 +0530

    update beforeTransfer hooks interface

--- contracts/child/IParentToken.sol
+++ contracts/child/IParentToken.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.5.2;
 //interface for parent contract of any child token
 
 
-contract IParentToken {
-  function beforeTransfer(address sender, address to, uint256 value) public returns(bool);
+interface IParentToken {
+  function beforeTransfer(address sender, address to, uint256 value) external returns(bool);
 }
--- contracts/child/ParentTokenMock.sol
+++ contracts/child/ParentTokenMock.sol
@@ -8,7 +8,7 @@ import "./IParentToken.sol";
 
 contract ParentTokenMock is IParentToken, Ownable {
   mapping (address => bool) isAllowed;
-  function beforeTransfer(address sender, address to, uint256 value) public returns(bool) {
+  function beforeTransfer(address sender, address to, uint256 value) external returns(bool) {
     return isAllowed[sender];
   }
 
