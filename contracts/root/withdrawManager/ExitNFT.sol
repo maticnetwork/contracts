84a4fcaae59124caed89224a23c1c9161c4d6b15
0xAshish <a4ashishrp@gmail.com>
2019-04-17 12:32:21 +0530

    move ExitNFT to withdraw manager

--- contracts/root/withdrawManager/ExitNFT.sol
+++ contracts/root/withdrawManager/ExitNFT.sol
@@ -1,26 +1,31 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.2;
 
 import { ERC721 } from "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
-
-import { ERC721Full } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import { ERC721Mintable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
-import { ERC721Burnable } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol";
-
-import { RootChainable } from "../mixin/RootChainable.sol";
+import { Registry } from '../Registry.sol';
 
 
-contract ExitNFT is RootChainable, ERC721Full {
-  constructor(string _name, string _symbol) public ERC721Full(_name, _symbol) {
+contract ExitNFT is ERC721 {
+  Registry internal registry;
 
+  modifier onlyWithdrawManager() {
+    require(
+      msg.sender == registry.getWithdrawManagerAddress(),
+      "UNAUTHORIZED_WITHDRAW_MANAGER_ONLY"
+    );
+    _;
   }
-  
-  function mint(address _owner, uint256 _tokenId) external onlyRootChain {
+
+  constructor(address _registry, string memory _name, string memory _symbol)
+    public
+  {
+    registry = Registry(_registry);
+  }
+
+  function mint(address _owner, uint256 _tokenId) external onlyWithdrawManager {
     _mint(_owner, _tokenId);
   }
 
-  function burn(address _owner, uint256 _tokenId) external onlyRootChain {
+  function burn(address _owner, uint256 _tokenId) external onlyWithdrawManager {
     _burn(_owner, _tokenId);
   }
-
 }
-
