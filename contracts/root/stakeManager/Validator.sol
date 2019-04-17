c0a63a89255bcc0a6629040ff19101dc1daa6ef8
0xAshish <a4ashishrp@gmail.com>
2019-04-17 12:25:22 +0530

    move stakemanager

---
+++ contracts/root/stakeManager/Validator.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.2;
+
+import { ERC721Full } from "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+contract Validator is ERC721Full {
+
+  //
+  // Storage
+  //
+
+}
