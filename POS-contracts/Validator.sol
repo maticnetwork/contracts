97a9d0e9611ab814b2ca1689d364371b5d217c41
0xAshish <a4ashishrp@gmail.com>
2019-06-21 17:54:32 +0530

    factory contract for new validator contract

--- POS-contracts/Validator.sol
+++ POS-contracts/Validator.sol
@@ -8,5 +8,37 @@ contract Validator is ERC721Full {
   //
   // Storage
   //
+  uint256[] public delegators;
+
+}
+
+
+contract ValidatorContract is ERC721Full {
+  uint256 public delegateAmount;
+  uint256[] public delegators;
+  // uint256 rewards;
+  uint256 public rewardRatio;
+  uint256 public slashingRatio;
+
+
+  function bond(uint256 delegatorId) public {
+    // is valid delegator
+    // isn't bonded
+    // lock , start
+  }
+
+  function unBond(uint256 delegatorId) public {
+    // award rewards according to rewardRatio
+    // start unbonding
+  }
+
+  function pullRewards() public {
+    // distribute delegator rewards first
+    // for each delegator reward, keep the rest
+  }
+
+  function slash() public {
+    // slash delegator according to slashingRatio
+  }
 
 }
