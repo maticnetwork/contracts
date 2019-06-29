529580c83971e1975c0ce177c497ea2eb10f256b
0xAshish <a4ashishrp@gmail.com>
2019-06-29 14:16:03 +0530

    restric calls from delegatro contract

--- POS-contracts/Validator.sol
+++ POS-contracts/Validator.sol
@@ -13,36 +13,55 @@ contract Validator is ERC721Full {
 }
 
 
-contract ValidatorContract is Ownable {
-  uint256 public delegateAmount;
+contract ValidatorContract is Ownable { // is rootchainable/stakeMgChainable
+  uint256 public delegatedAmount;
   uint256[] public delegators;
   // uint256 rewards;
-  address public Validator;
+  address public validator;
+  address public delegatorContract;
   uint256 public rewardRatio;
   uint256 public slashingRatio;
 
   constructor (address _owner) public {
-    Validator = _owner;
+    validator = _owner;
+  }
+
+  modifier onlyDelegatorContract() {
+    require(delegatorContract == msg.sender);
+    _;
   }
 
   function register() public onlyOwner {
 
   }
 
-  function bond(uint256 delegatorId) public {
+  function bond(uint256 delegatorId) public onlyDelegatorContract {
     // is valid delegator
     // isn't bonded
     // lock , start
+    delegators.push(delegatorId);
+    uint256 amount;
+    (, , , , amount) = Delegator(delegatorContract).delegators(delegatorId);
+    delegatedAmount += amount;
   }
 
-  function unBond(uint256 delegatorId) public {
+  function unBond(uint256 delegatorId, uint256 index) public onlyDelegatorContract {
     // update rewards according to rewardRatio
+    require(delegators[index] == delegatorId);
     // start unbonding
+    delegators[index] = delegators[delegators.length];
+    delete delegators[delegators.length];
   }
 
-  function getRewards() public {
+  function getRewards(uint256 delegatorId) public onlyDelegatorContract retruns(uint256) {
     // distribute delegator rewards first
     // for each delegator reward, keep the rest
+    uint256 startEpoch;
+    uint256 endEpoch;
+    (, startEpoch, endEpoch, , , ,) = Delegator(delegatorContract).delegators(delegatorId);
+    if (endEpoch) {
+      
+    }
   }
 
   function slash() public {
