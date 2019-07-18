0bdb6210702148e0e5ecb95a8310a99e7dd79940
0xAshish <a4ashishrp@gmail.com>
2019-07-18 12:36:45 +0530

    use new stakeManager

--- contracts/root/RootChain.sol
+++ contracts/root/RootChain.sol
@@ -5,7 +5,7 @@ import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 import { RootChainHeader, RootChainStorage } from "./RootChainStorage.sol";
-import { IStakeManager } from "./stakeManager/IStakeManager.sol";
+import { IStakeManager } from "../staking/IStakeManager.sol";
 import { Registry } from "../common/Registry.sol";
 
 
