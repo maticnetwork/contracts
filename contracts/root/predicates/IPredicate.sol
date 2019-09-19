a6d67b8ec99107b52b24b051e717ab3ff793805c
atvanguard <93arpit@gmail.com>
2019-09-19 08:31:47 +0530

    make chainId param uint16

--- contracts/root/predicates/IPredicate.sol
+++ contracts/root/predicates/IPredicate.sol
@@ -86,7 +86,7 @@ contract PredicateUtils is ExitsDataStructure {
     txHash = keccak256(RLPEncode.encodeList(rawTx));
     signer = ecrecover(
       txHash,
-      Common.getV(txList[6].toBytes(), Common.toUint8(networkId)),
+      Common.getV(txList[6].toBytes(), Common.toUint16(networkId)),
       bytes32(txList[7].toUint()),
       bytes32(txList[8].toUint())
     );
