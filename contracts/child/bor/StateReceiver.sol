76ac0da0861414fb8b101031759d91ebc9fd7d2f
0xAshish <a4ashishrp@gmail.com>
2019-10-17 02:06:11 +0530

    minor fix : undo  statkeReceiver

--- contracts/child/bor/StateReceiver.sol
+++ contracts/child/bor/StateReceiver.sol
@@ -1,6 +1,68 @@
 pragma solidity ^0.5.2;
 
-// StateReceiver represents interface to receive state
-interface StateReceiver {
-  function onStateReceive(uint256 id, bytes calldata data) external;
+import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import { RLPReader } from "solidity-rlp/contracts/RLPReader.sol";
+
+import { ValidatorVerifier } from "./ValidatorVerifier.sol";
+import { IStateReceiver } from "./IStateReceiver.sol";
+
+
+contract StateReceiver is ValidatorVerifier {
+  using SafeMath for uint256;
+  using RLPReader for bytes;
+  using RLPReader for RLPReader.RLPItem;
+
+  // states
+  mapping(uint256 => bool) public states;
+
+  // event for new state
+  event NewStateSynced(uint256 indexed id, address indexed contractAddress, bytes data);
+
+  // commit new state
+  function commitState(
+    bytes calldata vote,
+    bytes calldata sigs,
+    bytes calldata txBytes,
+    bytes calldata proof
+  ) external {
+    // validator validator set
+    validateValidatorSet(vote, sigs, txBytes, proof);
+
+    // check transaction data
+    RLPReader.RLPItem[] memory dataList = txBytes.toRlpItem().toList();
+
+    // dataList = [msg, signature, memo]
+    // msg = dataList[0]
+    dataList = dataList[0].toList();
+
+    // address proposer = dataList[0].toAddress();
+    // bytes32 txHash = dataList[1].toBytes32();
+    // uint256 logIndex = dataList[2].toUint();
+    uint256 stateId = dataList[3].toUint();
+    address contractAddress = dataList[4].toAddress();
+    bytes memory stateData = dataList[5].toBytes();
+
+    // state id must be same as state counter
+    require(states[stateId] == false, "Invalid state id");
+
+    // notify state receiver contract
+    if (isContract(contractAddress)) {
+      IStateReceiver(contractAddress).onStateReceive(stateId, stateData);
+    }
+
+    // set state
+    states[stateId] = true;
+
+    // emit event
+    emit NewStateSynced(stateId, contractAddress, stateData);
+  }
+
+  // check if address is contract
+  function isContract(address _addr) private view returns (bool){
+    uint32 size;
+    assembly {
+      size := extcodesize(_addr)
+    }
+    return (size > 0);
+  }
 }
