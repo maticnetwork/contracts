f853eabc98dd65485e75f700018c4d1f393db68d
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-10-31 13:49:26 +0530

    readme updated

--- truffle-config.js
+++ truffle-config.js
@@ -1,27 +1,27 @@
-require('babel-register')
-require('babel-polyfill')
+require("babel-register");
+require("babel-polyfill");
 
-var HDWalletProvider = require('truffle-hdwallet-provider')
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 const MNEMONIC =
   process.env.MNEMONIC ||
-  'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
-const API_KEY = process.env.API_KEY
+  "clock radar mass judge dismiss just intact mind resemble fringe diary casino";
+const API_KEY = process.env.API_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*', // match any network
+      network_id: "*", // match any network
       skipDryRun: true
     },
     matic_dev: {
-      host: 'localhost',
+      host: "localhost",
       port: 8546,
-      network_id: '*' // match any network
+      network_id: "*" // match any network
     },
     ropsten: {
       provider: () =>
@@ -41,7 +41,7 @@ module.exports = {
         return new HDWalletProvider(
           MNEMONIC,
           `https://kovan.infura.io/v3/${API_KEY}`
-        )
+        );
       },
       network_id: 42,
       gas: 8000000
@@ -51,14 +51,14 @@ module.exports = {
         return new HDWalletProvider(
           MNEMONIC,
           `https://mainnet.infura.io/v3/${API_KEY}`
-        )
+        );
       },
       network_id: 1,
       gas: 4000000
     },
-    matic: {
+    matic_testnet2: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, `https://testnet2.matic.network`)
+        return new HDWalletProvider(MNEMONIC, `https://testnet2.matic.network`);
       },
       network_id: 8995,
       gas: 8000000,
@@ -67,24 +67,24 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: '0.5.7',
+      version: "0.5.7",
       // docker: true,
       settings: {
         optimizer: {
           enabled: true,
           runs: 200
         },
-        evmVersion: 'constantinople'
+        evmVersion: "constantinople"
       }
     }
   },
   mocha: {
-    reporter: 'eth-gas-reporter',
+    reporter: "eth-gas-reporter",
     reporterOptions: {
-      currency: 'USD',
+      currency: "USD",
       gasPrice: 21,
-      outputFile: '/dev/null',
+      outputFile: "/dev/null",
       showTimeSpent: true
     }
   }
-}
+};
