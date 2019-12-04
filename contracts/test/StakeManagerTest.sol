b1f4400642f8b07cf65c0cd8431eb6621033c644
ashishrp <a4ashishrp@gmail.com>
2019-12-04 13:35:46 +0530

    minor fix

--- contracts/test/StakeManagerTest.sol
+++ contracts/test/StakeManagerTest.sol
@@ -12,7 +12,7 @@ contract StakeManagerTest is StakeManager {
     checkPointBlockInterval = 1;
   }
 
-  function checkSignatures(uint256 blockInterval, bytes32 voteHash, bytes32 stateRoot, bytes memory sigs) public onlyRootChain {
-    return;
+  function checkSignatures(uint256 blockInterval, bytes32 voteHash, bytes32 stateRoot, bytes memory sigs) public onlyRootChain returns(uint256) {
+    return checkpointReward; // for dummy tests return full reward
   }
 }
