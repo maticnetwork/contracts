eb3f8a94d5e9d9fa70fbbe2ed6ab40c7dec4f6ed
0xAshish <a4ashishrp@gmail.com>
2020-01-22 18:27:51 +0530

    add stakePower in checkSig

--- contracts/root/RootChain.sol
+++ contracts/root/RootChain.sol
@@ -47,8 +47,8 @@ contract RootChain is RootChainStorage, IRootChain {
     headerBlocks[_nextHeaderBlock] = headerBlock;
     // check if it is better to keep it in local storage instead
     IStakeManager stakeManager = IStakeManager(registry.getStakeManagerAddress());
-    uint256 _reward = 0;
-    // stakeManager.checkSignatures(headerBlock.end.sub(headerBlock.start), keccak256(vote), bytes32(extraData[4].toUint()), sigs);
+    // stakePower, blockInterval, voteHash, stateRoot, sigs
+    uint256 _reward = stakeManager.checkSignatures(extraData[5].toUint(), headerBlock.end.sub(headerBlock.start), keccak256(vote), bytes32(extraData[4].toUint()), sigs);
 
     emit NewHeaderBlock(headerBlock.proposer, _nextHeaderBlock, _reward, headerBlock.start, headerBlock.end, headerBlock.root);
     _nextHeaderBlock = _nextHeaderBlock.add(MAX_DEPOSITS);
