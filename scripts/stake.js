36f8043cbfaf88f3a4c037936e7bb43923e6a71b
atvanguard <93arpit@gmail.com>
2020-02-14 16:28:17 +0530

    Add functions to stake.js script

--- scripts/stake.js
+++ scripts/stake.js
@@ -3,9 +3,17 @@ const contracts = require('../contractAddresses.json')
 const RootToken = artifacts.require('TestToken')
 const StakeManager = artifacts.require('StakeManager')
 
-async function stake(address, amount) {
+async function getStakeManager() {
+  return StakeManager.at(contracts.root.StakeManager)
+}
+
+async function stake() {
+  const stakeFor = process.argv[6]
+  const amount = web3.utils.toWei(process.argv[7])
+  console.log(`Staking ${amount} for ${stakeFor}...`)
+
   const accounts = await web3.eth.getAccounts()
-  const stakeManager = await StakeManager.at(contracts.root.StakeManager)
+  const stakeManager = await getStakeManager()
   const rootToken = await RootToken.at(contracts.root.tokens.TestToken)
   console.log({ stakeManager: stakeManager.address, rootToken: rootToken.address })
   console.log('Sender accounts has a balanceOf', (await rootToken.balanceOf(accounts[0])).toString())
@@ -16,6 +24,7 @@ async function stake(address, amount) {
 }
 
 async function topUpForFee(address, amount) {
+  const stakeManager = await getStakeManager()
   const validatorId = await stakeManager.signerToValidator('0x25bE188468B1245Ab95037C238a24ee723493fE9')
   console.log(validatorId.toString())
   let r = await stakeManager.topUpForFee(validatorId.toString(), amount)
@@ -23,16 +32,25 @@ async function topUpForFee(address, amount) {
 }
 
 async function updateValidatorThreshold(number) {
+  const stakeManager = await getStakeManager()
+  console.log((await stakeManager.validatorThreshold()).toString())
   const r = await stakeManager.updateValidatorThreshold(number)
   console.log(r.tx)
+  console.log((await stakeManager.validatorThreshold()).toString())
+}
+
+async function updateCheckpointReward(reward) {
+  const stakeManager = await getStakeManager()
+  console.log((await stakeManager.CHECKPOINT_REWARD()).toString())
+  const r = await stakeManager.updateCheckpointReward(reward)
+  console.log(r.tx)
+  console.log((await stakeManager.CHECKPOINT_REWARD()).toString())
 }
 
 module.exports = async function(callback) {
-  const stakeFor = process.argv[6]
-  const amount = web3.utils.toWei(process.argv[7])
-  console.log(`Staking ${amount} for ${stakeFor}...`)
   try {
-    await stake(stakeFor, amount)
+    await stake()
+    // await updateCheckpointReward(web3.utils.toWei('10'))
   } catch(e) {
     // truffle exec <script> doesn't throw errors, so handling it in a verbose manner here
     console.log(e)
