bf8051acf2583f1dffd825d94776cf5ef3012bb4
atvanguard <93arpit@gmail.com>
2020-02-14 16:45:51 +0530

    updateRootCHain

--- contracts/root/depositManager/DepositManager.sol
+++ contracts/root/depositManager/DepositManager.sol
@@ -15,6 +15,7 @@ import {WETH} from "../../common/tokens/WETH.sol";
 import {IDepositManager} from "./IDepositManager.sol";
 import {DepositManagerStorage} from "./DepositManagerStorage.sol";
 import {StateSender} from "../stateSyncer/StateSender.sol";
+import {RootChain} from "../RootChain.sol";
 
 contract DepositManager is
     DepositManagerStorage,
@@ -214,5 +215,13 @@ contract DepositManager is
             abi.encode(_user, _token, _amountOrToken, _depositId)
         );
         emit NewDepositBlock(_user, _token, _amountOrToken, _depositId);
+    }
+
+    function updateRootChain(address _rootChain) public onlyOwner {
+        rootChain = RootChain(_rootChain);
+    }
+
+    function getRootChain() public view returns(address) {
+        return address(rootChain);
     }
 }
