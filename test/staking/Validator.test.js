8ea10890e4cecb0523817b63d6995ac74091c3b7
0xAshish <a4ashishrp@gmail.com>
2020-04-09 22:03:31 +0530

    fix tests

--- test/staking/Validator.test.js
+++ test/staking/Validator.test.js
@@ -37,6 +37,7 @@ contract('ValidatorShare', async function (accounts) {
     await stakeManager.updateValidatorThreshold(2)
     await stakeManager.changeRootChain(wallets[1].getAddressString())
     await stakeManager.setToken(stakeToken.address)
+    await stakeToken.mint(stakeManager.address, web3.utils.toWei('1000000'))// rewards amount
     await stakeToken.mint(
       user,
       amount
@@ -74,10 +75,10 @@ contract('ValidatorShare', async function (accounts) {
     let result = await validatorContract.buyVoucher(web3.utils.toWei('100'), {
       from: user
     })
+
     let logs = logDecoder.decodeLogs(result.receipt.rawLogs)
     assertBigNumberEquality(logs[3].args.amount, logs[3].args.tokens)
     assertBigNumberEquality(await validatorContract.exchangeRate(), web3.utils.toBN('100'))
-
     await checkPoint([wallets[1]], wallets[1], stakeManager, { totalStake: web3.utils.toWei('350') }, {
       from: wallets[1].getAddressString()
     })
