602af1111853b48223ef0deb0ab26dd6d9e90f1e
AyushKaul <ayushk.kaul3@gmail.com>
2020-04-29 14:05:37 +0530

    change: side-channel: submit header block

--- contracts/root/RootChain.sol
+++ contracts/root/RootChain.sol
@@ -8,6 +8,7 @@ import {RootChainHeader, RootChainStorage} from "./RootChainStorage.sol";
 import {IStakeManager} from "../staking/stakeManager/IStakeManager.sol";
 import {IRootChain} from "./IRootChain.sol";
 import {Registry} from "../common/Registry.sol";
+import "../common/lib/BytesLib.sol";
 
 contract RootChain is RootChainStorage, IRootChain {
     using SafeMath for uint256;
@@ -22,40 +23,85 @@ contract RootChain is RootChainStorage, IRootChain {
         _;
     }
 
+    // function submitHeaderBlock(
+    //     bytes calldata vote,
+    //     bytes calldata sigs,
+    //     bytes calldata txData
+    // ) external {
+    //     RLPReader.RLPItem[] memory dataList = vote.toRlpItem().toList();
+    //     require(
+    //         keccak256(dataList[0].toBytes()) == heimdallId,
+    //         "Chain ID is invalid"
+    //     );
+    //     require(dataList[1].toUint() == VOTE_TYPE, "Vote type is invalid");
+
+    //     // validate hash of txData was signed as part of the vote
+    //     require(
+    //         keccak256(dataList[4].toBytes()) ==
+    //             keccak256(abi.encodePacked(sha256(txData))),
+    //         "Extra data is invalid"
+    //     );
+
+    //     RLPReader.RLPItem[] memory extraData = txData.toRlpItem().toList();
+    //     extraData = extraData[0].toList();
+    //     RootChainHeader.HeaderBlock memory headerBlock = _buildHeaderBlock(
+    //         extraData
+    //     );
+    //     headerBlocks[_nextHeaderBlock] = headerBlock;
+    //     // check if it is better to keep it in local storage instead
+    //     IStakeManager stakeManager = IStakeManager(
+    //         registry.getStakeManagerAddress()
+    //     );
+    //     // blockInterval, voteHash, stateRoot, sigs
+    //     uint256 _reward = stakeManager.checkSignatures(
+    //         headerBlock.end.sub(headerBlock.start).add(1),
+    //         keccak256(vote),
+    //         bytes32(extraData[4].toUint()),
+    //         sigs
+    //     );
+    //     require(_reward != 0, "Invalid checkpoint");
+    //     emit NewHeaderBlock(
+    //         headerBlock.proposer,
+    //         _nextHeaderBlock,
+    //         _reward,
+    //         headerBlock.start,
+    //         headerBlock.end,
+    //         headerBlock.root
+    //     );
+    //     _nextHeaderBlock = _nextHeaderBlock.add(MAX_DEPOSITS);
+    //     _blockDepositId = 1;
+    // }
+
+    // side channel 
     function submitHeaderBlock(
-        bytes calldata vote,
-        bytes calldata sigs,
-        bytes calldata txData
+        bytes calldata data,
+        bytes calldata sigs
     ) external {
-        RLPReader.RLPItem[] memory dataList = vote.toRlpItem().toList();
-        require(
-            keccak256(dataList[0].toBytes()) == heimdallId,
-            "Chain ID is invalid"
-        );
-        require(dataList[1].toUint() == VOTE_TYPE, "Vote type is invalid");
+        (address proposer,
+        uint256 start,
+        uint256 end,
+        bytes32 rootHash,
+        bytes32 accountHash,
+        bytes32 _borChainID) = 
+        abi.decode(data, (address, uint256, uint256, bytes32, bytes32, bytes32));
+        // require(bytes32(uint(15001)) == _borChainID, "Invalid bor chain id");
 
-        // validate hash of txData was signed as part of the vote
-        require(
-            keccak256(dataList[4].toBytes()) ==
-                keccak256(abi.encodePacked(sha256(txData))),
-            "Extra data is invalid"
-        );
-
-        RLPReader.RLPItem[] memory extraData = txData.toRlpItem().toList();
-        extraData = extraData[0].toList();
         RootChainHeader.HeaderBlock memory headerBlock = _buildHeaderBlock(
-            extraData
+            proposer, start, end, rootHash
         );
         headerBlocks[_nextHeaderBlock] = headerBlock;
         // check if it is better to keep it in local storage instead
         IStakeManager stakeManager = IStakeManager(
             registry.getStakeManagerAddress()
         );
+
+        bytes memory votePrefix = hex'01';
+        
         // blockInterval, voteHash, stateRoot, sigs
         uint256 _reward = stakeManager.checkSignatures(
             headerBlock.end.sub(headerBlock.start).add(1),
-            keccak256(vote),
-            bytes32(extraData[4].toUint()),
+            keccak256(BytesLib.concat(votePrefix, data)), // prefix 01 to data
+            accountHash,
             sigs
         );
         require(_reward != 0, "Invalid checkpoint");
@@ -98,12 +144,15 @@ contract RootChain is RootChainStorage, IRootChain {
         return _nextHeaderBlock.sub(MAX_DEPOSITS);
     }
 
-    function _buildHeaderBlock(RLPReader.RLPItem[] memory dataList)
+    function _buildHeaderBlock(address proposer,
+    uint start,
+    uint end,
+    bytes32 rootHash)
         private
         view
         returns (HeaderBlock memory headerBlock)
     {
-        headerBlock.proposer = dataList[0].toAddress();
+        headerBlock.proposer = proposer;
         // Is this required? Why does a proposer need to be the sender? Think validator relay networks
         // require(msg.sender == dataList[0].toAddress(), "Invalid proposer");
 
@@ -116,14 +165,14 @@ contract RootChain is RootChainStorage, IRootChain {
             nextChildBlock = headerBlocks[currentHeaderBlock()].end + 1;
         }
         require(
-            nextChildBlock == dataList[1].toUint(),
+            nextChildBlock == start,
             "INCORRECT_START_BLOCK"
         );
         headerBlock.start = nextChildBlock;
-        headerBlock.end = dataList[2].toUint();
+        headerBlock.end = end;
 
         // toUintStrict returns the encoded uint. Encoded data must be padded to 32 bytes.
-        headerBlock.root = bytes32(dataList[3].toUintStrict());
+        headerBlock.root = rootHash;
         headerBlock.createdAt = now;
     }
 
