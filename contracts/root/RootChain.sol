a9f8235b54385475c2ba057972ea1a2ad480f4c9
AyushKaul <ayushk.kaul3@gmail.com>
2020-04-30 12:43:52 +0530

    minor: clean comments & chg chain id

--- contracts/root/RootChain.sol
+++ contracts/root/RootChain.sol
@@ -8,10 +8,9 @@ import {RootChainHeader, RootChainStorage} from "./RootChainStorage.sol";
 import {IStakeManager} from "../staking/stakeManager/IStakeManager.sol";
 import {IRootChain} from "./IRootChain.sol";
 import {Registry} from "../common/Registry.sol";
-import "../common/lib/BytesLib.sol";
+import {ChainIdMixin} from "../common/mixin/ChainIdMixin.sol";
 
-
-contract RootChain is RootChainStorage, IRootChain {
+contract RootChain is RootChainStorage, IRootChain, ChainIdMixin {
     using SafeMath for uint256;
     using RLPReader for bytes;
     using RLPReader for RLPReader.RLPItem;
@@ -24,56 +23,6 @@ contract RootChain is RootChainStorage, IRootChain {
         _;
     }
 
-    // function submitHeaderBlock(
-    //     bytes calldata vote,
-    //     bytes calldata sigs,
-    //     bytes calldata txData
-    // ) external {
-    //     RLPReader.RLPItem[] memory dataList = vote.toRlpItem().toList();
-    //     require(
-    //         keccak256(dataList[0].toBytes()) == heimdallId,
-    //         "Chain ID is invalid"
-    //     );
-    //     require(dataList[1].toUint() == VOTE_TYPE, "Vote type is invalid");
-
-    //     // validate hash of txData was signed as part of the vote
-    //     require(
-    //         keccak256(dataList[4].toBytes()) ==
-    //             keccak256(abi.encodePacked(sha256(txData))),
-    //         "Extra data is invalid"
-    //     );
-
-    //     RLPReader.RLPItem[] memory extraData = txData.toRlpItem().toList();
-    //     extraData = extraData[0].toList();
-    //     RootChainHeader.HeaderBlock memory headerBlock = _buildHeaderBlock(
-    //         extraData
-    //     );
-    //     headerBlocks[_nextHeaderBlock] = headerBlock;
-    //     // check if it is better to keep it in local storage instead
-    //     IStakeManager stakeManager = IStakeManager(
-    //         registry.getStakeManagerAddress()
-    //     );
-    //     // blockInterval, voteHash, stateRoot, sigs
-    //     uint256 _reward = stakeManager.checkSignatures(
-    //         headerBlock.end.sub(headerBlock.start).add(1),
-    //         keccak256(vote),
-    //         bytes32(extraData[4].toUint()),
-    //         sigs
-    //     );
-    //     require(_reward != 0, "Invalid checkpoint");
-    //     emit NewHeaderBlock(
-    //         headerBlock.proposer,
-    //         _nextHeaderBlock,
-    //         _reward,
-    //         headerBlock.start,
-    //         headerBlock.end,
-    //         headerBlock.root
-    //     );
-    //     _nextHeaderBlock = _nextHeaderBlock.add(MAX_DEPOSITS);
-    //     _blockDepositId = 1;
-    // }
-
-    // side channel
     function submitHeaderBlock(bytes calldata data, bytes calldata sigs)
         external
     {
@@ -88,7 +37,7 @@ contract RootChain is RootChainStorage, IRootChain {
             data,
             (address, uint256, uint256, bytes32, bytes32, bytes32)
         );
-        require(bytes32(uint256(15001)) == _borChainID, "Invalid bor chain id");
+        require(bytes32(CHAINID) == _borChainID, "Invalid bor chain id");
 
         require(
             _buildHeaderBlock(proposer, start, end, rootHash),
