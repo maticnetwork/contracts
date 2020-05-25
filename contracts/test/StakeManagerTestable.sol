943fb3129acdf8335ae9e7f25e7165f14d2046ac
0xAshish <a4ashishrp@gmail.com>
2020-05-25 17:25:33 +0530

    hack for contract size issue, drain function is not needed in that contract anyway

--- contracts/test/StakeManagerTestable.sol
+++ contracts/test/StakeManagerTestable.sol
@@ -1,9 +1,17 @@
 pragma solidity ^0.5.2;
 
-import { StakeManager } from "../staking/stakeManager/StakeManager.sol";
+import {StakeManager} from "../staking/stakeManager/StakeManager.sol";
+
 
 contract StakeManagerTestable is StakeManager {
-    function advanceEpoch(uint delta) public {
+    function advanceEpoch(uint256 delta) public {
         currentEpoch = currentEpoch.add(delta);
     }
+
+    function drainValidatorShares(
+        uint256 validatorId,
+        address token,
+        address payable destination,
+        uint256 amount
+    ) external {} // Note: hack to compensate contract size
 }
