dc1b86c41b5717008229b277cf4f873bacef2f0b
0xAshish <a4ashishrp@gmail.com>
2020-05-25 15:32:55 +0530

    fix code size issue

--- contracts/staking/stakeManager/StakeManager.sol
+++ contracts/staking/stakeManager/StakeManager.sol
@@ -694,13 +694,13 @@ contract StakeManager is IStakeManager, StakeManagerStorage {
         require(token.transfer(destination, amount), "Drain failed");
     }
 
-    function reinitialize(
-        address _NFTContract,
-        address _stakingLogger,
-        address _validatorShareFactory
-    ) external onlyGovernance {
-        NFTContract = StakingNFT(_NFTContract);
-        logger = StakingInfo(_stakingLogger);
-        factory = ValidatorShareFactory(_validatorShareFactory);
-    }
+    // function reinitialize(
+    //     address _NFTContract,
+    //     address _stakingLogger,
+    //     address _validatorShareFactory
+    // ) external onlyGovernance {
+    //     NFTContract = StakingNFT(_NFTContract);
+    //     logger = StakingInfo(_stakingLogger);
+    //     factory = ValidatorShareFactory(_validatorShareFactory);
+    // }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -21,7 +21,7 @@ module.exports = {
       port: 9545,
       network_id: '*', // match any network
       skipDryRun: true,
-      gas: 7000000
+      gas: 9000000
     },
     bor: {
       host: 'localhost',
