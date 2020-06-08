0e709b0accc0d7289b6339b8cc9ffc90390e101b
Denis Ermolin <denis.ermolin@matic.network>
2020-06-08 12:58:44 +0700

    new: Upgradable proxy using unstructured strorage

---
+++ contracts/common/misc/UpgradableProxy.sol
@@ -0,0 +1,94 @@
+pragma solidity ^0.5.2;
+
+import {DelegateProxy} from "./DelegateProxy.sol";
+
+contract UpgradableProxy is DelegateProxy {
+    event ProxyUpdated(address indexed _new, address indexed _old);
+    event OwnerUpdate(address _new, address _old);
+
+    bytes32 constant IMPLEMENTATION_SLOT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffbf;
+    bytes32 constant OWNER_SLOT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa9;
+
+    constructor(address _proxyTo) public {
+        setOwner(msg.sender);
+        setImplementation(_proxyTo);
+    }
+
+    function() external payable {
+        // require(currentContract != 0, "If app code has not been set yet, do not call");
+        // Todo: filter out some calls or handle in the end fallback
+        delegatedFwd(loadImplementation(), msg.data);
+    }
+
+    modifier onlyProxyOwner() {
+        require(loadOwner() == msg.sender);
+        _;
+    }
+
+    function owner() external view returns(address) {
+        return loadOwner();
+    }
+
+    function loadOwner() internal view returns(address) {
+        address _owner;
+        bytes32 position = OWNER_SLOT;
+        assembly {
+            _owner := sload(position)
+        }
+        return _owner;
+    }
+
+    function implementation() external view returns (address) {
+        return loadImplementation();
+    }
+
+    function loadImplementation() internal view returns(address) {
+        address _impl;
+        bytes32 position = IMPLEMENTATION_SLOT;
+        assembly {
+            _impl := sload(position)
+        }
+        return _impl;
+    }
+
+    function transferOwnership(address newOwner) public onlyProxyOwner {
+        require(newOwner != address(0), "ZERO_ADDRESS");
+        emit OwnerUpdate(newOwner, loadOwner());
+        setOwner(newOwner);
+    }
+
+    function setOwner(address newOwner) private {
+        bytes32 position = OWNER_SLOT;
+        assembly {
+            sstore(position, newOwner)
+        }
+    }
+
+    function updateImplementation(address _newProxyTo) public onlyProxyOwner {
+        require(_newProxyTo != address(0x0), "INVALID_PROXY_ADDRESS");
+        require(isContract(_newProxyTo), "DESTINATION_ADDRESS_IS_NOT_A_CONTRACT");
+
+        emit ProxyUpdated(_newProxyTo, loadImplementation());
+        
+        setImplementation(_newProxyTo);
+    }
+
+    function setImplementation(address _newProxyTo) private {
+        bytes32 position = IMPLEMENTATION_SLOT;
+        assembly {
+            sstore(position, _newProxyTo)
+        }
+    }
+    
+    function isContract(address _target) internal view returns (bool) {
+        if (_target == address(0)) {
+            return false;
+        }
+
+        uint256 size;
+        assembly {
+            size := extcodesize(_target)
+        }
+        return size > 0;
+    }
+}
