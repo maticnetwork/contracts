64b4052611c97eb4baedfe2cc7e8e776761009ae
Denis Ermolin <denis.ermolin@matic.network>
2020-06-08 21:39:53 +0700

    new: Initializable test

--- contracts/test/Proxy/ProxyTestImpl.sol
+++ contracts/test/Proxy/ProxyTestImpl.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.2;
 
-contract ProxyTestImpl {
+import {Initializable} from "../../common/mixin/Initializable.sol";
+
+contract ProxyTestImpl is Initializable {
     uint256 public a = 1;
     uint256 public b = 2;
     uint256 public ctorInit;
@@ -9,7 +11,7 @@ contract ProxyTestImpl {
         ctorInit = 3;
     }
 
-    function init() public {
+    function init() public initializer {
         a = 1;
         b = 2;
     }
