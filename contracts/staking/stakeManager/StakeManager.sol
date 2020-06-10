5f9ee2e2183c2f94f986377d35ea5f2bdb63f5d0
Denis Ermolin <denis.ermolin@matic.network>
2020-06-10 14:27:06 +0700

    refactor: re-use rewards liquidation code in unstake()
    and withdrawRewards()

--- contracts/staking/stakeManager/StakeManager.sol
+++ contracts/staking/stakeManager/StakeManager.sol
@@ -328,17 +328,21 @@ contract StakeManager is IStakeManager, StakeManagerStorage {
         logger.logRestaked(validatorId, validators[validatorId].amount, newTotalStaked);
     }
 
+    function _liquidateRewards(uint256 validatorId, uint256 reward) private {
+        totalRewardsLiquidated = totalRewardsLiquidated.add(reward);
+        validators[validatorId].reward = 0;
+        require(token.transfer(msg.sender, reward), "Insufficent rewards");
+        logger.logClaimRewards(validatorId, reward, totalRewardsLiquidated);
+    }
+
     function withdrawRewards(uint256 validatorId) public onlyStaker(validatorId) {
-        uint256 amount = validators[validatorId].reward;
+        uint256 reward = validators[validatorId].reward;
         address contractAddr = validators[validatorId].contractAddress;
         if (contractAddr != address(0x0)) {
-            amount = amount.add(IValidatorShare(contractAddr).withdrawRewardsValidator());
+            reward = reward.add(IValidatorShare(contractAddr).withdrawRewardsValidator());
         }
 
-        totalRewardsLiquidated = totalRewardsLiquidated.add(amount);
-        validators[validatorId].reward = 0;
-        require(token.transfer(msg.sender, amount), "Insufficent rewards");
-        logger.logClaimRewards(validatorId, amount, totalRewardsLiquidated);
+        _liquidateRewards(validatorId, reward);
     }
 
     function getValidatorId(address user) public view returns (uint256) {
@@ -660,8 +664,8 @@ contract StakeManager is IStakeManager, StakeManagerStorage {
             delegationAmount = int256(validatorShare.lockContract());
         }
 
-        validators[validatorId].reward = 0;
-        require(token.transfer(validator, rewards), "Rewards transfer failed");
+        _liquidateRewards(validatorId, rewards);
+
         //  update future
         updateTimeLine(exitEpoch, -(int256(amount) + delegationAmount), -1);
 
