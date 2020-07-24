28ade4c2c6cedbd1b07009faef528564bcc0aa74
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-07-24 16:42:59 +0530

    new: reddit poc

--- migrations/2_deploy_root_contracts.js
+++ migrations/2_deploy_root_contracts.js
@@ -1,61 +1,69 @@
 // Deploy minimal number of contracts to link the libraries with the contracts
 
-const bluebird = require('bluebird')
+const bluebird = require("bluebird");
 
 const SafeMath = artifacts.require(
-  'openzeppelin-solidity/contracts/math/SafeMath.sol'
-)
+  "openzeppelin-solidity/contracts/math/SafeMath.sol"
+);
 
-const RLPReader = artifacts.require('solidity-rlp/contracts/RLPReader.sol')
+const RLPReader = artifacts.require("solidity-rlp/contracts/RLPReader.sol");
 
-const BytesLib = artifacts.require('BytesLib')
-const Common = artifacts.require('Common')
-const ECVerify = artifacts.require('ECVerify')
-const Merkle = artifacts.require('Merkle')
-const MerklePatriciaProof = artifacts.require('MerklePatriciaProof')
-const PriorityQueue = artifacts.require('PriorityQueue')
-const RLPEncode = artifacts.require('RLPEncode')
+const BytesLib = artifacts.require("BytesLib");
+const Common = artifacts.require("Common");
+const ECVerify = artifacts.require("ECVerify");
+const Merkle = artifacts.require("Merkle");
+const MerklePatriciaProof = artifacts.require("MerklePatriciaProof");
+const PriorityQueue = artifacts.require("PriorityQueue");
+const RLPEncode = artifacts.require("RLPEncode");
 
-const Registry = artifacts.require('Registry')
-const Governance = artifacts.require('Governance')
-const RootChain = artifacts.require('RootChain')
-const DepositManager = artifacts.require('DepositManager')
-const WithdrawManager = artifacts.require('WithdrawManager')
-const StakeManager = artifacts.require('StakeManager')
-const StakeManagerProxy = artifacts.require('StakeManagerProxy')
-const SlashingManager = artifacts.require('SlashingManager')
-const StakingInfo = artifacts.require('StakingInfo')
-const StakingNFT = artifacts.require('StakingNFT')
-const TestToken = artifacts.require('TestToken')
-const ValidatorShareFactory = artifacts.require('ValidatorShareFactory')
-const ERC20Predicate = artifacts.require('ERC20Predicate')
-const ERC721Predicate = artifacts.require('ERC721Predicate')
-const MintableERC721Predicate = artifacts.require('MintableERC721Predicate')
-const Marketplace = artifacts.require('Marketplace')
-const MarketplacePredicate = artifacts.require('MarketplacePredicate')
-const MarketplacePredicateTest = artifacts.require('MarketplacePredicateTest')
-const TransferWithSigPredicate = artifacts.require('TransferWithSigPredicate')
-const TransferWithSigUtils = artifacts.require('TransferWithSigUtils')
+const Registry = artifacts.require("Registry");
+const Governance = artifacts.require("Governance");
+const RootChain = artifacts.require("RootChain");
+const DepositManager = artifacts.require("DepositManager");
+const WithdrawManager = artifacts.require("WithdrawManager");
+const StakeManager = artifacts.require("StakeManager");
+const StakeManagerProxy = artifacts.require("StakeManagerProxy");
+const SlashingManager = artifacts.require("SlashingManager");
+const StakingInfo = artifacts.require("StakingInfo");
+const StakingNFT = artifacts.require("StakingNFT");
+const TestToken = artifacts.require("TestToken");
+const ValidatorShareFactory = artifacts.require("ValidatorShareFactory");
+const ERC20Predicate = artifacts.require("ERC20Predicate");
+const RedditPredicate = artifacts.require("RedditPredicate");
+const ERC721Predicate = artifacts.require("ERC721Predicate");
+const MintableERC721Predicate = artifacts.require("MintableERC721Predicate");
+const Marketplace = artifacts.require("Marketplace");
+const MarketplacePredicate = artifacts.require("MarketplacePredicate");
+const MarketplacePredicateTest = artifacts.require("MarketplacePredicateTest");
+const TransferWithSigPredicate = artifacts.require("TransferWithSigPredicate");
+const TransferWithSigUtils = artifacts.require("TransferWithSigUtils");
 
-const StakeManagerTestable = artifacts.require('StakeManagerTestable')
-const StakeManagerTest = artifacts.require('StakeManagerTest')
+const StakeManagerTestable = artifacts.require("StakeManagerTestable");
+const StakeManagerTest = artifacts.require("StakeManagerTest");
 
 const libDeps = [
   {
     lib: BytesLib,
-    contracts: [WithdrawManager, ERC20Predicate, ERC721Predicate, MintableERC721Predicate]
+    contracts: [
+      WithdrawManager,
+      ERC20Predicate,
+      RedditPredicate,
+      ERC721Predicate,
+      MintableERC721Predicate,
+    ],
   },
   {
     lib: Common,
     contracts: [
       WithdrawManager,
       ERC20Predicate,
+      RedditPredicate,
       ERC721Predicate,
       MintableERC721Predicate,
       MarketplacePredicate,
       MarketplacePredicateTest,
-      TransferWithSigPredicate
-    ]
+      TransferWithSigPredicate,
+    ],
   },
   {
     lib: ECVerify,
@@ -65,39 +73,47 @@ const libDeps = [
       StakeManagerTestable,
       MarketplacePredicate,
       MarketplacePredicateTest,
-      TransferWithSigPredicate
-    ]
+      TransferWithSigPredicate,
+    ],
   },
   {
     lib: Merkle,
     contracts: [
       WithdrawManager,
       ERC20Predicate,
+      RedditPredicate,
       ERC721Predicate,
       MintableERC721Predicate,
       StakeManager,
       StakeManagerTestable,
-      StakeManagerTest
-    ]
+      StakeManagerTest,
+    ],
   },
   {
     lib: MerklePatriciaProof,
-    contracts: [WithdrawManager, ERC20Predicate, ERC721Predicate, MintableERC721Predicate]
+    contracts: [
+      WithdrawManager,
+      ERC20Predicate,
+      RedditPredicate,
+      ERC721Predicate,
+      MintableERC721Predicate,
+    ],
   },
   {
     lib: PriorityQueue,
-    contracts: [WithdrawManager]
+    contracts: [WithdrawManager],
   },
   {
     lib: RLPEncode,
     contracts: [
       WithdrawManager,
       ERC20Predicate,
+      RedditPredicate,
       ERC721Predicate,
       MintableERC721Predicate,
       MarketplacePredicate,
-      MarketplacePredicateTest
-    ]
+      MarketplacePredicateTest,
+    ],
   },
   {
     lib: RLPReader,
@@ -105,17 +121,19 @@ const libDeps = [
       RootChain,
       StakeManager,
       ERC20Predicate,
+      RedditPredicate,
       ERC721Predicate,
       MintableERC721Predicate,
       MarketplacePredicate,
-      MarketplacePredicateTest
-    ]
+      MarketplacePredicateTest,
+    ],
   },
   {
     lib: SafeMath,
     contracts: [
       RootChain,
       ERC20Predicate,
+      RedditPredicate,
       ERC721Predicate,
       MintableERC721Predicate,
       MarketplacePredicate,
@@ -123,70 +141,86 @@ const libDeps = [
       TransferWithSigPredicate,
       StakeManager,
       SlashingManager,
-      StakingInfo
-    ]
+      StakingInfo,
+    ],
   },
   {
     lib: SafeMath,
-    contracts: [RootChain, ERC20Predicate]
+    contracts: [RootChain, ERC20Predicate, RedditPredicate],
   },
   {
     lib: TransferWithSigUtils,
     contracts: [
       TransferWithSigPredicate,
       MarketplacePredicate,
-      MarketplacePredicateTest
-    ]
-  }
-]
+      MarketplacePredicateTest,
+    ],
+  },
+];
 
-module.exports = async function(deployer, network, accounts) {
-  deployer.then(async() => {
-    console.log('linking libs...')
-    await bluebird.map(libDeps, async e => {
-      await deployer.deploy(e.lib)
-      deployer.link(e.lib, e.contracts)
-    })
+module.exports = async function (deployer, network, accounts) {
+  deployer.then(async () => {
+    console.log("linking libs...");
+    await bluebird.map(libDeps, async (e) => {
+      await deployer.deploy(e.lib);
+      deployer.link(e.lib, e.contracts);
+    });
 
-    console.log('deploying contracts...')
-    await deployer.deploy(Governance)
-    await deployer.deploy(Registry, Governance.address)
-    await deployer.deploy(ValidatorShareFactory)
-    await deployer.deploy(TestToken, 'Matic Test', 'MATICTEST')
-    await deployer.deploy(StakingInfo, Registry.address)
-    await deployer.deploy(StakingNFT, 'Matic Validator', 'MV')
+    console.log("deploying contracts...");
+    await deployer.deploy(Governance);
+    await deployer.deploy(Registry, Governance.address);
+    await deployer.deploy(ValidatorShareFactory);
+    await deployer.deploy(TestToken, "Matic Test", "MATICTEST");
+    await deployer.deploy(StakingInfo, Registry.address);
+    await deployer.deploy(StakingNFT, "Matic Validator", "MV");
     await Promise.all([
-      deployer.deploy(RootChain, Registry.address, 'heimdall-P5rXwg'),
+      deployer.deploy(RootChain, Registry.address, "heimdall-P5rXwg"),
 
       deployer.deploy(WithdrawManager),
-      deployer.deploy(DepositManager)
-    ])
+      deployer.deploy(DepositManager),
+    ]);
 
-    await deployer.deploy(StakeManagerTestable)
-    await deployer.deploy(StakeManagerTest)
-    const stakeManager = await deployer.deploy(StakeManager)
-    const proxy = await deployer.deploy(StakeManagerProxy, '0x0000000000000000000000000000000000000000')
+    await deployer.deploy(StakeManagerTestable);
+    await deployer.deploy(StakeManagerTest);
+    const stakeManager = await deployer.deploy(StakeManager);
+    const proxy = await deployer.deploy(
+      StakeManagerProxy,
+      "0x0000000000000000000000000000000000000000"
+    );
     await proxy.updateAndCall(
       StakeManager.address,
-      stakeManager.contract.methods.initialize(
-        Registry.address,
-        RootChain.address,
-        TestToken.address,
-        StakingNFT.address,
-        StakingInfo.address,
-        ValidatorShareFactory.address,
-        Governance.address,
-        accounts[0]
-      ).encodeABI()
-    )
+      stakeManager.contract.methods
+        .initialize(
+          Registry.address,
+          RootChain.address,
+          TestToken.address,
+          StakingNFT.address,
+          StakingInfo.address,
+          ValidatorShareFactory.address,
+          Governance.address,
+          accounts[0]
+        )
+        .encodeABI()
+    );
 
-    await deployer.deploy(SlashingManager, Registry.address, StakingInfo.address, 'heimdall-P5rXwg')
-    let stakingNFT = await StakingNFT.deployed()
-    await stakingNFT.transferOwnership(StakeManagerProxy.address)
+    await deployer.deploy(
+      SlashingManager,
+      Registry.address,
+      StakingInfo.address,
+      "heimdall-P5rXwg"
+    );
+    let stakingNFT = await StakingNFT.deployed();
+    await stakingNFT.transferOwnership(StakeManagerProxy.address);
 
     await Promise.all([
       deployer.deploy(
         ERC20Predicate,
+        WithdrawManager.address,
+        DepositManager.address,
+        Registry.address
+      ),
+      deployer.deploy(
+        RedditPredicate,
         WithdrawManager.address,
         DepositManager.address,
         Registry.address
@@ -214,7 +248,7 @@ module.exports = async function(deployer, network, accounts) {
         RootChain.address,
         WithdrawManager.address,
         Registry.address
-      )
-    ])
-  })
-}
+      ),
+    ]);
+  });
+};
