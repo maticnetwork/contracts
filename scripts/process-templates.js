038cce5504f1c5e655d3c690da04dc90c02420fb
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-08-07 15:36:25 +0530

    chg: default chain id in template process, readme update

--- scripts/process-templates.js
+++ scripts/process-templates.js
@@ -5,20 +5,20 @@ const fs = require("fs");
 const path = require("path");
 
 program.version("0.0.1");
-program.option("-c, --bor-chain-id <bor-chain-id>", "Bor chain id", "15001");
+program.option("-c, --bor-chain-id <bor-chain-id>", "Bor chain id", "80001");
 program.parse(process.argv);
 
 //joining path of directory
 const directoryPath = path.join(__dirname, "..", "**/*.template");
 //passsing directoryPath and callback function
-glob(directoryPath, function(err, files) {
+glob(directoryPath, function (err, files) {
   //handling error
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
 
   //listing all files using forEach
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     // Do whatever you want to do with the file
     const borChainIdHex = parseInt(program.borChainId, 10)
       .toString(16)
@@ -27,7 +27,7 @@ glob(directoryPath, function(err, files) {
     const data = {
       borChainId: program.borChainId,
       borChainIdHex:
-        borChainIdHex.length % 2 !== 0 ? `0${borChainIdHex}` : borChainIdHex
+        borChainIdHex.length % 2 !== 0 ? `0${borChainIdHex}` : borChainIdHex,
     };
 
     const templateString = fs.readFileSync(file).toString();
