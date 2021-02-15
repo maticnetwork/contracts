0c0e95f7f8815ff33e83d59ba556eb198e3c9a8d
Denis Ermolin <denis.ermolin@matic.network>
2021-02-15 19:40:13 +0700

    fix: storage layout

--- contracts/staking/validatorShare/ValidatorShare.sol
+++ contracts/staking/validatorShare/ValidatorShare.sol
@@ -26,9 +26,9 @@ contract ValidatorShare is IValidatorShare, ERC20NonTradable, OwnableLockable, I
     uint256 public minAmount;
 
     uint256 public totalStake_deprecated;
-    uint256 public activeAmount_deprecated;
-
     uint256 public rewardPerShare;
+    uint256 public activeAmount_deprecated;
+    
     bool public delegation;
 
     uint256 public withdrawPool;
@@ -76,7 +76,7 @@ contract ValidatorShare is IValidatorShare, ERC20NonTradable, OwnableLockable, I
 
     function withdrawExchangeRate() public view returns (uint256) {
         uint256 precision = _getRatePrecision();
-        if (validatorId < 8) {
+        if (validatorId < 9) {
             // fix of potentially broken withdrawals for future unbonding
             // foundation validators have no slashing enabled and thus we can return default exchange rate
             // because without slashing rate will stay constant
