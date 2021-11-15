8623e384be40c949248e6cb36bb5d669892b6797
Denis Ermolin <denis.ermolin@matic.network>
2021-11-15 18:39:02 +0700

    chg: generate typechain bindings after installation

--- tasks/compile.ts
+++ tasks/compile.ts
@@ -1,9 +1,7 @@
 
 import { task } from 'hardhat/config'
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names'
-import { TASKS } from './task-names'
 
 task(TASK_COMPILE, async function(args, { run }, runSuper) {
   await runSuper(args)
-  await run(TASKS.VERIFY_STORAGE, args)
 })
