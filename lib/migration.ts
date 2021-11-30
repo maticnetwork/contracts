a7ae7277876094d6c84c6163458871e29f3ef3dc
Denis Ermolin <denis.ermolin@matic.network>
2021-11-30 16:14:29 +0700

    upd: more proxies migrations

--- lib/migration.ts
+++ lib/migration.ts
@@ -1,5 +1,10 @@
 import chalk from 'chalk'
-import { cc, GovernanceRepositoryLink } from '../lib'
+import { ReleaseRegistry, cc, GovernanceRepositoryLink } from './index'
+
+import { StakeManagerContract } from '../typechain'
+
+import '@nomiclabs/hardhat-truffle5'
+import { Artifacts } from 'hardhat/types'
 
 export async function printGovernanceUpdateCommand(target: string, contract: string, data: string) {
   cc.log(`Use this command in our ${GovernanceRepositoryLink} repository to generate multisig transaction data:`)
@@ -15,4 +20,22 @@ export async function printProxyUpdateCommand(proxy: string, implementation: str
     const encodedCall = `npx hardhat encode-update-proxy-data --proxy ${proxy} --implementation ${implementation}`
 
     await cc.intendGroup(async() => cc.log(chalk.bgBlack(chalk.yellowBright(encodedCall))))
+}
+
+export async function deployProxyImplementation(artifacts: Artifacts, contractName: string, network: string, from: string) {
+  const stakeManager = artifacts.require(contractName) as StakeManagerContract
+  const instance = await stakeManager.new({ from })
+  const deployedAddress = instance.address
+
+  const registry = new ReleaseRegistry(network)
+  await registry.load()
+
+  cc.logLn()
+  await cc.intendGroup(async() => {
+    cc.logLn(`Deployed ${contractName} at ${deployedAddress}`)
+
+    // generate calldata for multisig
+    printProxyUpdateCommand(registry.getAddress(`${contractName}Proxy`), registry.getAddress(contractName))
+    cc.logLn()
+  }, 'Deployment result:')
 }
--- migrations/EventsHub.ts
+++ migrations/EventsHub.ts
@@ -1,8 +1,6 @@
 import { deployProxyImplementation } from '../lib'
-import '@nomiclabs/hardhat-truffle5'
 import { Artifacts } from 'hardhat/types'
-import { RegistryContract } from 'typechain'
 
 export async function deploy(artifacts: Artifacts, network: string, from: string) {
-  await deployProxyImplementation(artifacts, 'StakeManager', network, from)
+  await deployProxyImplementation(artifacts, 'EventsHub', network, from)
 }
