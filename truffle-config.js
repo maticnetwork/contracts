a1bdb02be1395845c27fc22caa802f7debe18b62
Arpit Temani <temaniarpit27@gmail.com>
2022-06-01 16:19:28 +0530

    remove docker for compiling

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,7 @@ var HDWalletProvider = require('truffle-hdwallet-provider')
 
 const MNEMONIC =
   process.env.MNEMONIC ||
-  'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
+  'truck gallery select material claim elephant pear dog knock kitchen runway juice'
 const API_KEY = process.env.API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
@@ -18,10 +18,10 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 9545,
+      port: 7545,
       network_id: '*', // match any network
       skipDryRun: true,
-      gas: 7000000
+      gas: 6721975
     },
     bor: {
       provider: () =>
@@ -76,7 +76,6 @@ module.exports = {
   compilers: {
     solc: {
       version: '0.5.17',
-      docker: true,
       parser: 'solcjs',
       settings: {
         optimizer: {
