233f2d22bf8a63cc50d308afa074a9bc38e5e53d
Jesse Lee <gatsby1118@gmail.com>
2022-10-31 22:02:05 -0400

    safemath fix

--- contracts/common/gnosis/GnosisSafe.sol
+++ contracts/common/gnosis/GnosisSafe.sol
@@ -4,6 +4,9 @@
 
 pragma solidity >=0.5.0 <0.7.0;
 
+
+import {SafeMath} from "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 /// @title SelfAuthorized - authorizes current contract to perform actions
 /// @author Richard Meissner - <richard@gnosis.pm>
 contract SelfAuthorized {
@@ -606,72 +609,6 @@ contract ISignatureValidator is ISignatureValidatorConstants {
         returns (bytes4);
 }
 
-
-/**
- * @title SafeMath2
- * @dev Math operations with safety checks that revert on error
- * TODO: remove once open zeppelin update to solc 0.5.0
- */
-library SafeMath2 {
-
-  /**
-  * @dev Multiplies two numbers, reverts on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    uint256 c = a * b;
-    require(c / a == b);
-
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b > 0); // Solidity only automatically asserts when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-    return c;
-  }
-
-  /**
-  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
-    uint256 c = a - b;
-
-    return c;
-  }
-
-  /**
-  * @dev Adds two numbers, reverts on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c >= a);
-
-    return c;
-  }
-
-  /**
-  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
-  * reverts when dividing by zero.
-  */
-  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b != 0);
-    return a % b;
-  }
-}
-
 /// @title Gnosis Safe - A multisignature wallet with support for confirmations using signed messages based on ERC191.
 /// @author Stefan George - <stefan@gnosis.io>
 /// @author Richard Meissner - <richard@gnosis.io>
@@ -679,7 +616,7 @@ library SafeMath2 {
 contract GnosisSafe
     is MasterCopy, ModuleManager, OwnerManager, SignatureDecoder, SecuredTokenTransfer, ISignatureValidatorConstants, FallbackManager {
 
-    using SafeMath2 for uint256;
+    using SafeMath for uint256;
 
     string public constant NAME = "Gnosis Safe";
     string public constant VERSION = "1.1.1";
