4bfa48e9008fc0e138e6970260889856832f4dbe
leovct <l009.vincent@gmail.com>
2023-11-27 10:32:04 +0100

    chore: nit

--- migrations/5_deploy_child_contracts.js
+++ migrations/5_deploy_child_contracts.js
@@ -27,45 +27,45 @@ module.exports = async function(deployer, _, _) {
     // Deploy MaticWeth (ERC20) child contract and its proxy.
     // Initialize the contract, update the child chain and map the token with its root contract.
     const childMaticWethProxified = await ChildERC20Proxified.new()
-    console.log('childMaticWethProxified deployed')
+    console.log('Child MaticWethProxified contract deployed')
     const childMaticWethProxy = await ChildTokenProxy.new(childMaticWethProxified.address)
-    console.log('childMaticWethProxy deployed')
+    console.log('Child MaticWeth proxy contract deployed')
     const childMaticWeth = await ChildERC20Proxified.at(childMaticWethProxy.address)
 
     await childMaticWeth.initialize(contractAddresses.root.tokens.MaticWeth, 'Eth on Matic', 'ETH', 18)
-    console.log('childMaticWeth initialized')
+    console.log('Child MaticWeth contract initialized')
     await childMaticWeth.changeChildChain(childChain.address)
-    console.log('childMaticWeth child chain updated')
+    console.log('Child MaticWeth child chain updated')
     await childChain.mapToken(contractAddresses.root.tokens.MaticWeth, childMaticWeth.address, false)
-    console.log('root and child maticWeth mapped')
+    console.log('Root and child MaticWeth contracts mapped')
 
     // Same thing for TestToken (ERC20).
     const childTestTokenProxified = await ChildERC20Proxified.new()
-    console.log('childTestTokenProxified deployed')
+    console.log('Child TestTokenProxified contract deployed')
     const childTestTokenProxy = await ChildTokenProxy.new(childTestTokenProxified.address)
-    console.log('childTestTokenProxy deployed')
+    console.log('Child TestToken proxy contract deployed')
     const childTestToken = await ChildERC20Proxified.at(childTestTokenProxy.address)
 
     await childTestToken.initialize(contractAddresses.root.tokens.TestToken, 'Test Token', 'TST', 18)
-    console.log('childTestToken initialized')
+    console.log('Child TestToken contract initialized')
     await childTestToken.changeChildChain(childChain.address)
-    console.log('childTestToken child chain updated')
+    console.log('Child TestToken child chain updated')
     await childChain.mapToken(contractAddresses.root.tokens.TestToken, childTestToken.address, false)
-    console.log('root and child testToken mapped')
+    console.log('Root and child TestToken contracts mapped')
 
     // Same thing for TestERC721.
     const childTestERC721Proxified = await ChildERC721Proxified.new()
-    console.log('childTestERC721Proxified deployed')
+    console.log('Child TestERC721Proxified contract deployed')
     const childTestERC721Proxy = await ChildTokenProxy.new(childTestERC721Proxified.address)
-    console.log('childTestERC721Proxy deployed')
+    console.log('Child TestERC721 proxy contract deployed')
     const childTestERC721 = await ChildERC721Proxified.at(childTestERC721Proxy.address)
 
     await childTestERC721.initialize(contractAddresses.root.tokens.RootERC721, 'Test ERC721', 'TST721')
-    console.log('childTestERC721 initialized')
+    console.log('Child TestERC721 contract initialized')
     await childTestERC721.changeChildChain(childChain.address)
-    console.log('childTestERC721 child chain updated')
+    console.log('Child TestERC721 child chain updated')
     await childChain.mapToken(contractAddresses.root.tokens.RootERC721, childTestERC721.address, true) // ERC721
-    console.log('root and child testERC721 mapped')
+    console.log('Root and child testERC721 contracts mapped')
 
     // Initialize and map MaticToken.
     const maticToken = await MRC20.at('0x0000000000000000000000000000000000001010')
