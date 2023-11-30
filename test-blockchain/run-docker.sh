c5cc058b35a9adc41060503cb09c0efa3791e0da
leovct <l009.vincent@gmail.com>
2023-11-30 16:52:40 +0100

    chore: typo

--- test-blockchain/run-docker.sh
+++ test-blockchain/run-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --name bor-test -it -d -p 8545:8545 -v $(pwd):/bordata 0xpolygon/bor:v1.2.0 /bin/sh -c "cd /bordata; sh start.sh"
+docker run --name bor-test -it -d -p 8545:8545 -v $(pwd):/bordata 0xpolygon/bor:1.2.0 /bin/sh -c "cd /bordata; sh start.sh"
